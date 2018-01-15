85f240322c917889473af24d2742f43fc0e28bbe
Mo <mojtaba_amiri@hotmail.co.uk>
2018-01-15 02:24:26 +0000

    cleaner ish script..

--- Vagrantfile
+++ Vagrantfile
@@ -65,5 +65,7 @@ Vagrant.configure("2") do |config|
 
   # documentation for more information about their specific syntax and use.
   # config.vm.provision "shell", path: "https://gist.githubusercontent.com/m0jtaba/52c5cef9ad92d753cea0c8bdd7986c0c/raw/8dfb80d1ebfaa268b3f11121b0475fc0c0eee379/stretchyVagrant.sh"
+  config.vm.provision "file", source: "elasticsearch.repo", destination: "/home/vagrant/elasticsearch.repo"
   config.vm.provision "shell", path: "stretchyVagrant.sh"
+  
 end
--- stretchyVagrant.sh
+++ stretchyVagrant.sh
@@ -10,36 +10,36 @@ sudo yum install -y net-tools
 #Epel release
 if (yum list installed | grep epel-release.noarch);
 then
-    echo "epel-relase already installed.";
+    echo -e "\e[42mepel-relase already installed.\e[0m";
 else
-    echo "Installing epel-release....";
+    echo -e "\e[45mInstalling epel-release....\e[0m";
     sudo yum install -y epel-release;
 fi
 
 #htop
 if (yum list installed | grep htop.x86_64);
 then
-    echo "htop.x86_64 already installed.";
+    echo -e "\e[42mhtop.x86_64 already installed.\e[0m";
 else
-    echo "Installing htop.x86_64....";
+   echo -e "\e[45mInstalling htop.x86_64....\e[0m";
     sudo yum install -y htop;
 fi
 
 #wget
 if (yum list installed | grep wget.x86_64);
 then
-    echo "wget.x86_64 already installed.";
+    echo -e "\e[42mwget.x86_64 already installed.\e[0m";
 else
-    echo "Installing wget.x86_64....";
+    echo -e "\e[45mInstalling wget.x86_64....\e[0m";
     sudo yum install -y wget;
 fi
 
 #JAVA
 if (yum list installed | grep jdk1.8.x86_64);
 then
-    echo "Java 1.8 is already installed.";
+    echo -e "\e[42mJava 1.8 already installed.\e[0m";
 else
-    echo "installing java 1.8..........";
+    echo -e "\e[45mInstalling java 1.8..........\e[0m";
     wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.rpm";
     sudo yum -y localinstall -y jdk-8u151-linux-x64.rpm;
 fi
@@ -47,22 +47,15 @@ fi
 #Elasticsearch
 if (yum list installed | grep elasticsearch.noarch);
 then
-    echo "elasticsearch is already installed.";
+    echo -e "\e[42melasticsearch is already installed.\e[0m";
 else
-    echo "installing elasticsearch...........";
+    echo -e "\e[45mInstalling elasticsearch...........\e[0m";
     sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch;
-    sudo echo "[elasticsearch-5.x]
-    name=Elasticsearch repository for 5.x packages
-    baseurl=https://artifacts.elastic.co/packages/5.x/yum
-    gpgcheck=1
-    gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-    enabled=1
-    autorefresh=1
-    type=rpm-md" > /etc/yum.repos.d/elasticsearch.repo;
+    sudo mv /home/vagrant/elasticsearch.repo /etc/yum.repos.d/
     sudo yum -y install elasticsearch;
     sudo /bin/systemctl daemon-reload;
     sudo /bin/systemctl enable elasticsearch.service;
-    echo "changing elastic to ip to 192.168.56.10";
+    echo -e "\e[45mchanging elastic ip to 192.168.56.10\e[0m";
     #TODO: use AWK to get the ip addr and use a variable instead of static
     sudo sed -i 's/#network.host: 192.168.0.1/network.host: 192.168.56.10/' /etc/elasticsearch/elasticsearch.yml;
     sudo systemctl start elasticsearch.service;
@@ -71,13 +64,13 @@ fi
 #Kibana
 if (yum list installed | grep kibana.x86_64);
 then
-    echo "Kibana is already installed.";
+    echo -e "\e[42mKibana is already installed.\e[0m";
 else
-    echo "Installing kibana..........";
+    echo -e "\e[45mInstalling kibana..........\e[0m";
     sudo yum -y install kibana;
     sudo /bin/systemctl daemon-reload;
     sudo /bin/systemctl enable kibana.service;
-     #TODO: use AWK to get the ip addr and use a variable instead of static
+    #TODO: use AWK to get the ip addr and use a variable instead of static
     sudo sed -i 's/#server.host: "localhost"/server.host: 192.168.56.10/' /etc/kibana/kibana.yml;
     sudo sed -i 's;#elasticsearch.url: "http://localhost:9200";elasticsearch.url: http://192.168.56.10:9200;' /etc/kibana/kibana.yml;
     sudo systemctl start kibana.service;
